928b2a3abdb760f71d216163b7efcb05423fde9d
sayaka.kita <sayaka.kita.1988@gmail.com>
2018-07-01 12:16:53 +0900

    helloプロジェクトを新規作成

---
+++ hello/hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by 喜多さやか on 2018/07/01.
+//  Copyright © 2018年 sayaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
