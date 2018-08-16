94f92e069d081c91fe571d80403b489b9da749b8
sayaka.kita <sayaka.kita.1988@gmail.com>
2018-08-16 21:25:05 +0900

    LuckyLunchプロジェクト新規作成

---
+++ LuckyLunch/LuckyLunch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LuckyLunch
+//
+//  Created by sayaka on 2018/08/16.
+//  Copyright © 2018年 sayaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
